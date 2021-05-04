f4875f18d3137221e15742ac2544eccc372533a5
armornik <kd200681@mail.ru>
2021-05-04 22:59:35 +0300

    Check login link on main pages

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,19 @@
-def test_guest_can_go_to_login_page(browser):
+from selenium.webdriver import Remote as RemoteWebDriver
+
+from .pages.main_page import MainPage
+
+
+def test_guest_can_go_to_login_page(browser: RemoteWebDriver):
+    """Check test use command 'pytest -v --tb=line --language=en test_main_page.py'"""
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)   # init Page Object, pass the driver instance to the constructor and url address
+    page.open()                      # open page
+    page.go_to_login_page()          # execute the page method - go to the login page
+
+
+def test_guest_should_see_login_link(browser: RemoteWebDriver):
+    """Check login link"""
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
