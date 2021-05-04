f2c0992dbdbddeffe84f25214f8ec6718e6ea631
armornik <kd200681@mail.ru>
2021-05-04 23:16:36 +0300

    use locators

--- test_main_page.py
+++ test_main_page.py
@@ -6,6 +6,7 @@ from .pages.main_page import MainPage
 def test_guest_can_go_to_login_page(browser: RemoteWebDriver):
     """Check test use command 'pytest -v --tb=line --language=en test_main_page.py'"""
     link = "http://selenium1py.pythonanywhere.com/"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)   # init Page Object, pass the driver instance to the constructor and url address
     page.open()                      # open page
     page.go_to_login_page()          # execute the page method - go to the login page
