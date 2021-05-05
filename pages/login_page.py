f2a5e96bb1d1563e2ecfbd9e59e5f2ec44bf234c
armornik <kd200681@mail.ru>
2021-05-05 09:58:33 +0300

    Check login page

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        """Check URL contains 'login'"""
+        assert "login" in self.browser.current_url, "URL not login page"
+
+    def should_be_login_form(self):
+        """Check page contains login form"""
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        """Check page contains register form"""
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,7 @@
+# pytest -v --tb=line --language=en test_main_page.py
 from selenium.webdriver import Remote as RemoteWebDriver
 
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -10,6 +12,8 @@ def test_guest_can_go_to_login_page(browser: RemoteWebDriver):
     page = MainPage(browser, link)   # init Page Object, pass the driver instance to the constructor and url address
     page.open()                      # open page
     page.go_to_login_page()          # execute the page method - go to the login page
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser: RemoteWebDriver):
